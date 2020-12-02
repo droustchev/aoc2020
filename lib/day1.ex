7d7358b010912e9c09327ebdc23a4c15f34adf30
Dimitar Roustchev <dimitar@pagerduty.com>
2020-12-02 02:10:01 -0500

    Rewrite day1-1 non-rec

--- lib/day1.ex
+++ lib/day1.ex
@@ -7,14 +7,10 @@ defmodule Day1 do
 
   def part1 do
     for m <- input() do
-      n = input()
-          |> Enum.filter(&(&1 + m == 2020))
-
-      case n do
-        [head|_] ->
-          IO.puts("Seems like it is #{m} + #{head}")
-          IO.puts("The multiple is #{m*head}")
-        [] -> :nope
+      n = 2020 - m
+      case Enum.member?(input(), n) do
+        true -> IO.puts(m * n)
+          _ -> :nope
       end
     end
   end
